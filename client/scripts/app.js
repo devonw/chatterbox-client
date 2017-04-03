f52f11755c22b74341a4d0dcde10bd005fb20924
Devon Weldon <deveathbo@gmail.com>
2017-04-03 13:41:18 -0700

    First commit, some initial things

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
-// YOUR CODE HERE:
+var app = {
+  init: function() {
+
+  },
+  send: function(message) {
+    $.ajax({
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+      
+    });
+  },
+  fetch: function() {
+  	$.ajax({
+  	  type: 'GET'
+  	});
+
+  },
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+
+  renderMessage: function(message) {
+    var userName = message.userName;
+    var message = message.text;
+    var room = message.room;
+    var newMessage = $('<div>' + userName + '<div><div>' + message + '<div>');
+    $('#chats').append(newMessage);
+  },
+
+  renderRoom: function() {
+
+  }
+};
+
+
